1446fdd098ddcb1077da038f2762d0183b51ba59
Liu Jingkai <rock_jk@msn.com>
2021-05-18 11:53:12 +0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Guadoo on 2021/5/18.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
